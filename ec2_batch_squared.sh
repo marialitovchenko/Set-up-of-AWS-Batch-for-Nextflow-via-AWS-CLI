e942cc871a1850f6b28b1e2cb56942514c657a80
Maria <maria.litovchenko@gmail.com>
2024-06-14 10:11:47 +0100

    Added extra policies and modified script to be more straightforward

--- ec2_batch_squared.sh
+++ ec2_batch_squared.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 AWS_PROFILE_NAME="ngs_workflows_dev"
 AWS_REGION_NAME="eu-west-2"
-BASE_AMI_IMAGE_ID="ami-06373f703eb245f45" # Amazon Linux 2023 AMI
-INSTANCE_TYPE="t3.2xlarge"
-EXECUTING_USER="mlitovchenko"
-S3_BUCKET_NAME="s3-test"
+LAUNCHER_INSTANCE_TYPE="t2.medium"
+EXECUTING_USER="ml"
+S3_BUCKET_NAME="nf-test"
 
-IAM_USER_NAME=nextflow-programmatic-access-$EXECUTING_USER
-IAM_GROUP_NAME=nextflow-group-$EXECUTING_USER
-EC2_NAME="nextflow-EC2-"$EXECUTING_USER
+IAM_USER_NAME=nf-program-access-$EXECUTING_USER
+IAM_GROUP_NAME=nf-group-$EXECUTING_USER
+EC2_NAME="nf-EC2-"$EXECUTING_USER
+
+KEY_PAIR_NAME=$EC2_NAME"-keypair"
+CUSTOM_AMI_NAME="ami-nf-aws-batch-"$EXECUTING_USER
+BATCH_COMPUTE_ENV_NAME="nf-aws-batch-"$EXECUTING_USER
+BATCH_JOB_QUEUE_NAME="nf-queue-"$EXECUTING_USER
 
 IAM_ROLE_NAME=AmazonEC2SpotFleetRole
-KEY_PAIR_NAME=$EC2_NAME"-KeyPair"
 EC2_DEFAULT_USER_NAME="ec2-user"
-CUSTOM_AMI_NAME="ami_nextflow_aws_batch_"$EXECUTING_USER
-BATCH_COMPUTE_ENV_NAME="nextflow_aws_batch_"$EXECUTING_USER
-BATCH_JOB_QUEUE_NAME="nextflow_queue_"$EXECUTING_USER
 
 # set AWS profile to the one with suitable permissions
 export AWS_PROFILE=$AWS_PROFILE_NAME
@@ -23,146 +23,75 @@ export AWS_PROFILE=$AWS_PROFILE_NAME
 export AWS_REGION=$AWS_REGION_NAME
 
 # Step 1 Setting up a Nextflow user with IAM
-# https://docs.aws.amazon.com/IAM/latest/UserGuide/id_users_create.html#id_users_create_cliwpsapi
-# create a programmatic user
+# create a user
 aws iam create-user --user-name $IAM_USER_NAME
-read -r IAM_USER_ID <<<$(aws iam list-users --query "Users[*].[UserName, UserId]" \
-                                            --output text | grep $IAM_USER_NAME | cut -f2)
-
-# give the user programmatic access 
-read -r ACCESS_KEY_ID ACCESS_KEY_SECRET <<<$(aws iam create-access-key \
-                                                 --user-name $IAM_USER_NAME \
-                                                 --output text | \
-                                                 cut -f2,4)
 
 # create a user group
-aws iam create-group      --group-name $IAM_GROUP_NAME
-aws iam add-user-to-group --user-name  $IAM_USER_NAME \
-                          --group-name $IAM_GROUP_NAME
-
+aws iam create-group --group-name $IAM_GROUP_NAME
+aws iam add-user-to-group --user-name $IAM_USER_NAME \
+    --group-name $IAM_GROUP_NAME
 # attach access policies to user group
 aws iam attach-group-policy --group-name $IAM_GROUP_NAME \
-                            --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess
+    --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess
 aws iam attach-group-policy --group-name $IAM_GROUP_NAME \
-                            --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+    --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 aws iam attach-group-policy --group-name $IAM_GROUP_NAME \
-                            --policy-arn arn:aws:iam::aws:policy/AWSBatchFullAccess 
+    --policy-arn arn:aws:iam::aws:policy/AWSBatchFullAccess
+aws iam attach-group-policy --group-name $IAM_GROUP_NAME \
+    --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
 
 # create permission roles for running AWS Batch
 aws iam create-role --role-name $IAM_ROLE_NAME \
-                    --assume-role-policy-document '{
-                            "Version":"2012-10-17",
-                            "Statement":[
-                                {
-                                "Sid":"",
-                                "Effect":"Allow",
-                                "Principal": {
-                                    "Service":"spotfleet.amazonaws.com"
-                                },
-                                "Action":"sts:AssumeRole"
-                                }
-                            ]
-                            }'
-aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole \
-                           --role-name $IAM_ROLE_NAME
+    --assume-role-policy-document '{
+                                    "Version":"2012-10-17",
+                                    "Statement":[
+                                        {
+                                            "Sid":"",
+                                            "Effect":"Allow",
+                                            "Principal": {
+                                                "Service":"spotfleet.amazonaws.com"
+                                            },
+                                            "Action":"sts:AssumeRole"
+                                        }]
+                                    }'
+aws iam attach-role-policy --role-name $IAM_ROLE_NAME \
+    --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole
+aws iam attach-role-policy --role-name $IAM_ROLE_NAME \
+    --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role
+aws iam attach-role-policy --role-name $IAM_ROLE_NAME \
+    --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
 
-                            --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role
-
-
-# Step 2 Build a custom Amazon Machine Image
-aws ssm get-parameters --names /aws/service/ecs/optimized-ami/amazon-linux-2023/recommended
-
+# Step 2: create security group
 # get VPC ID and PUBLIC(!) subnet ID. If subnet is not public and doesn't allow
 # map of public ip on launch, we won't be able to ssh to it.
-read -r SUBNET_ID VPC_ID <<<$(aws ec2 describe-subnets --filters "Name=map-public-ip-on-launch,Values=true" \
-                                                       --query "Subnets[*].[SubnetId, VpcId]" \
-                                                       --output text | head -1)
-
+read -r SUBNET_ID VPC_ID <<<"$(aws ec2 describe-subnets --filters 'Name=map-public-ip-on-launch,Values=true' \
+    --query 'Subnets[*].[SubnetId, VpcId]' \
+    --output text | head -1)"
 # create a security group
-read -r SECURITY_GROUP_ID <<<$(aws ec2 create-security-group --group-name $EC2_NAME \
-                                                             --description $EC2_NAME" security group" \
-                                                             --vpc-id $VPC_ID \
-                                                             --output text)
-# add rules to security group: allow inbound traffic on TCP port 22 to support SSH connections
-aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID \
-                                         --protocol tcp \
-                                         --port 22 --cidr "0.0.0.0/0"
-aws ec2 authorize-security-group-egress --group-id $SECURITY_GROUP_ID \
-                                        --protocol tcp \
-                                        --port 22 --cidr "0.0.0.0/0"
-aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID \
-                                         --protocol tcp \
-                                         --port 80 --cidr "0.0.0.0/0"
-aws ec2 authorize-security-group-egress --group-id $SECURITY_GROUP_ID \
-                                        --protocol tcp \
-                                        --port 80 --cidr "0.0.0.0/0"
+read -r SECURITY_GROUP_ID <<<"$(aws ec2 create-security-group --group-name $EC2_NAME \
+    --description $EC2_NAME" security group" \
+    --vpc-id $VPC_ID \
+    --output text)"
+# add rules to security group: allow inbound traffic on TCP port 22 to support
+# SSH connections
+aws ec2 authorize-security-group-ingress --group-id "$SECURITY_GROUP_ID" \
+    --protocol tcp --port 22 --cidr "0.0.0.0/0"
+aws ec2 authorize-security-group-egress --group-id "$SECURITY_GROUP_ID" \
+    --protocol tcp --port 22 --cidr "0.0.0.0/0"
+aws ec2 authorize-security-group-ingress --group-id "$SECURITY_GROUP_ID" \
+    --protocol tcp --port 80 --cidr "0.0.0.0/0"
+aws ec2 authorize-security-group-egress --group-id "$SECURITY_GROUP_ID" \
+    --protocol tcp --port 80 --cidr "0.0.0.0/0"
 
+# Step 3: define & create AWS Batch compute environment
 # create a key pair
 aws ec2 create-key-pair --key-name $KEY_PAIR_NAME \
-                        --query 'KeyMaterial' \
-                        --output text > $KEY_PAIR_NAME".pem"
+    --query 'KeyMaterial' --output text >$KEY_PAIR_NAME".pem"
 chmod 400 $KEY_PAIR_NAME".pem"
-
-# create an instance
-read -r INSTANCE_ID <<<$(aws ec2 run-instances --image-id $BASE_AMI_IMAGE_ID \
-                                               --count 1 \
-                                               --instance-type $INSTANCE_TYPE \
-                                               --key-name $KEY_PAIR_NAME \
-                                               --security-group-ids $SECURITY_GROUP_ID \
-                                               --subnet-id $SUBNET_ID \
-                                               --block-device-mappings  '[
-                                                    {
-                                                        "DeviceName": "/dev/xvda",
-                                                        "Ebs": {
-                                                            "VolumeSize": 100,
-                                                            "VolumeType": "standard",
-                                                            "DeleteOnTermination": true
-                                                        }
-                                                    }
-                                                ]' \
-                                               --output text \
-                                               --query "Instances[*].[InstanceId]")
-# rename instance
-aws ec2 create-tags --resources $INSTANCE_ID \
-                    --tags Key=Name,Value=$EC2_NAME
-# wait for the instance to be running
-aws ec2 wait instance-status-ok --instance-ids $INSTANCE_ID
-# get public ID address of an instance
-read -r PUBLIC_DNS_NAME <<<$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-                                                        --query 'Reservations[].Instances[].PublicDnsName' \
-                                                        --output text)
-
-# login into EC2
-ssh -i $KEY_PAIR_NAME".pem" $EC2_DEFAULT_USER_NAME"@"$PUBLIC_DNS_NAME
-# install docker on EC2
-# add Docker's official GPG key:
-sudo yum update -y
-sudo yum install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-# add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo yum update -y
-# install the Docker packages.
-sudo yum install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-# install aws cli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-exit
-
-read -r CUSTOM_AMI_ID <<<$(aws ec2 create-image --instance-id $INSTANCE_ID \
-                                                --name $CUSTOM_AMI_NAME \
-                                                --no-reboot \
-                                                --output text)
-
-# Step 3 Define AWS Batch compute environment
+# find out AMI ID
+read -r CUSTOM_AMI_ID <<<"$(aws ec2 describe-images --owners self \
+    --filters 'Name=name,Values='$CUSTOM_AMI_NAME \
+    --output text | cut -f8)"
 batch_compute_config='{
                         "type": "SPOT",
                         "allocationStrategy": "SPOT_CAPACITY_OPTIMIZED",
@@ -171,36 +100,24 @@ batch_compute_config='{
                         "maxvCpus": 1024,
                         "instanceTypes": ["optimal"],
                         "imageId": "'$CUSTOM_AMI_ID'",
-                        "subnets": ["'$SUBNET_ID'", "subnet-05534e5ba9e1491c1", "subnet-092cd8753dd23c57e"],
+                        "subnets": ["'$SUBNET_ID'"],
                         "securityGroupIds": ["'$SECURITY_GROUP_ID'"],
                         "ec2KeyPair": "'$KEY_PAIR_NAME'",
                         "instanceRole": "ecsInstanceRole",
                         "bidPercentage": 99,
                         "spotIamFleetRole": "AmazonEC2SpotFleetRole"
                      }'
-# batch_compute_config='{
-#                         "type": "EC2",
-#                         "allocationStrategy": "BEST_FIT_PROGRESSIVE",
-#                         "minvCpus": 0,
-#                         "desiredvCpus": 0,
-#                         "maxvCpus": 128,
-#                         "instanceTypes": ["optimal"],
-#                         "imageId": "ami-02bdc46746b0733da",
-#                         "subnets": ["'$SUBNET_ID'", "subnet-05534e5ba9e1491c1", "subnet-092cd8753dd23c57e"],
-#                         "securityGroupIds": ["'$SECURITY_GROUP_ID'"],
-#                         "ec2KeyPair": "'$KEY_PAIR_NAME'",
-#                         "instanceRole": "arn:aws:iam::352918899944:instance-profile/ecsInstanceRole"
-#                      }'
 # create the compute environment
-aws batch create-compute-environment --compute-environment-name $BATCH_COMPUTE_ENV_NAME \
-                                     --state ENABLED \
-                                     --type MANAGED \
-                                     --compute-resources $batch_compute_config
-# Step 4 Create an AWS Batch Job queue
+aws batch create-compute-environment \
+    --compute-environment-name $BATCH_COMPUTE_ENV_NAME \
+    --state ENABLED --type MANAGED \
+    --compute-resources "$batch_compute_config"
+sleep 30
+
+# Step 4: create an AWS Batch job queue
 aws batch create-job-queue --job-queue-name $BATCH_JOB_QUEUE_NAME \
-                           --state ENABLED \
-                           --priority 1 \
-                           --compute-environment-order '
+    --state ENABLED --priority 1 \
+    --compute-environment-order '
                            [
                                 {
                                     "order": 1,
@@ -208,9 +125,71 @@ aws batch create-job-queue --job-queue-name $BATCH_JOB_QUEUE_NAME \
                                 }
                             ]'
 
-# Step 5 Set up an S3 Bucket for data access
+# Step 5: create up an S3 bucket for data storage
 aws s3api create-bucket --bucket $EXECUTING_USER'-'$S3_BUCKET_NAME \
-                        --create-bucket-configuration LocationConstraint=$AWS_REGION_NAME
+    --create-bucket-configuration LocationConstraint=$AWS_REGION_NAME
+
+# Step 6: create EC2 which will hold nextflow launcher. We will use custom AMI
+# created previously as base, and will install nextflow on it.
+read -r INSTANCE_ID <<<"$(aws ec2 run-instances --image-id "$CUSTOM_AMI_ID" \
+    --count 1 \
+    --instance-type $LAUNCHER_INSTANCE_TYPE \
+    --key-name $KEY_PAIR_NAME \
+    --security-group-ids "$SECURITY_GROUP_ID" \
+    --subnet-id "$SUBNET_ID" \
+    --block-device-mappings '[
+                                {
+                                    "DeviceName": "/dev/xvda",
+                                    "Ebs": {
+                                        "VolumeSize": 100,
+                                        "VolumeType": "standard",
+                                        "DeleteOnTermination": true
+                                    }
+                                }
+                            ]' \
+    --output text \
+    --query 'Instances[*].[InstanceId]')"
+# rename instance
+aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=$EC2_NAME
+# wait for the instance to be running
+aws ec2 wait instance-status-ok --instance-ids $INSTANCE_ID
+# get public ID address of an instance
+read -r PUBLIC_DNS_NAME <<<"$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+    --query 'Reservations[].Instances[].PublicDnsName' \
+    --output text)"
+
+# Step 6: inform user about the next steps
+echo "AWS BATCH for Nextflow is successfully set up."
+echo "EC2 machine which will hold Nextflow launcher is successfully set up."
+echo "Here are your next steps: "
+echo "  Retrieve & record the access key and access key secret for ""$IAM_USER_NAME"" by running:"
 
 # login to ec2
 aws configure
+
+sudo yum install git-all -y
+sudo yum install zip unzip -y
+
+curl -s "https://get.sdkman.io" | bash
+source "/home/ec2-user/.sdkman/bin/sdkman-init.sh"
+sdk install java 21.0.3-tem
+
+curl -s https://get.nextflow.io | bash
+chmod +x nextflow
+sudo mv nextflow /usr/local/bin
+
+nextflow pull rnaseq-nf
+nextflow -C nextflow.config run rnaseq-nf \
+    -profile batch \
+    --output s3://ml-nf-test/rnaseq-nf/results/ \
+    -resume
+
+# NOT NEEDED
+read -r IAM_USER_ID <<<"$(aws iam list-users --query 'Users[*].[UserName, UserId]' \
+    --output text | grep $IAM_USER_NAME | cut -f2)"
+
+# get access keys
+read -r ACCESS_KEY_ID ACCESS_KEY_SECRET <<<"$(aws iam create-access-key \
+    --user-name $IAM_USER_NAME \
+    --output text |
+    cut -f2,4)"
